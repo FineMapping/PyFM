28ca6d14f126776fbcbdf6240edaf4523d48fc19
Ishaan   Gupta <ishaangupta@Ishaans-MBP-7.lan>
2024-03-02 14:56:08 -0800

    Added priors, posterior, PIP, and selection to complete basic model

--- src/fm.py
+++ src/fm.py
@@ -4,11 +4,14 @@ import argparse
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # from utils import
-from Data import Data
-from BF import outputBF
-from Configurations import CONFIGS_IMPL_MAP
+from data import Data
+from bf import calculate_BFs, calculate_scores
+from configurations import CONFIGS_IMPL_MAP
+from priors import calculate_priors
+from selection import get_rhos, get_marginal_pip, print_marginal_pips, print_rhos
 
 
 def parse_args():
@@ -46,6 +49,11 @@ def parse_args():
         help=f"choose one causal configurations exploration method from {CONFIGS_IMPL_MAP.keys()}",
         default="AllConfigurations",
     )
+    parser.add_argument(
+        "--rho",
+        help="the cutoff for rho-confidence set",
+        default=0.95,
+    )
     params = parser.parse_args()
     # TODO: Move the code below into a function that can be called if this project is imported as a python library
     n = int(params.sample_number)
@@ -69,6 +77,8 @@ def parse_args():
     max_causal = int(params.max_causal)
 
     configs_method = params.configs_method
+
+    rho = float(params.rho)
     return (
         data,
         n,
@@ -78,6 +88,7 @@ def parse_args():
         max_causal,
         configs_method,
         approx_bf,
+        rho,
         outdir,
     )
 
@@ -92,9 +103,12 @@ if __name__ == "__main__":
         max_causal,
         configs_method,
         approx_bf,
+        rho,
         outdir,
     ) = parse_args()
-    BF = outputBF(
+
+    # set scores = log(BF)
+    config_scores, best_configs = calculate_BFs(
         data,
         n,
         pve_for_prior,
@@ -105,3 +119,28 @@ if __name__ == "__main__":
         approx_bf,
         os.path.join(outdir, "BF.tsv"),
     )
+
+    # calculate priors
+    n_causal2log_prior = calculate_priors(data.m, max_causal)
+
+    # set scores = log (BF x prior/max_BF)
+    max_BF = max([config_scores[k][tuple(best_configs[k])] for k in config_scores])
+    config_scores, total_score = calculate_scores(
+        config_scores, n_causal2log_prior, max_BF
+    )
+
+    # set null_model_score
+    null_model_score = 10 ** (
+        n_causal2log_prior[0] - max_BF
+    )  # BF = 1, log_BF = 0 for null model
+    total_score += null_model_score
+    print("totalscore = ", total_score)
+
+    # get marginal PIP for each variant
+    marginal_pip, ranking = get_marginal_pip(data.m, config_scores, total_score)
+
+    print_marginal_pips(marginal_pip, ranking, data, os.path.join(outdir, "pips.tsv"))
+
+    # rho-confidence set
+    rho_scores = get_rhos(ranking, config_scores, max_causal)
+    print_rhos(rho_scores, ranking, data, os.path.join(outdir, "rhos.tsv"))
