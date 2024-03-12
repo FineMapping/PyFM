79275baa48cb6a330dc5039a63946c08c775c6fd
Zhaoyang-Jia <z4jia@ucsd.edu>
2024-03-11 20:37:46 -0700

    no pve file

---
+++ test.sh
@@ -0,0 +1,5 @@
+python src/fm.py \
+  -z example/eQTL/region.LOC284581.chr1.205831207.205865215.dosage.p1e-12.nopve.z \
+  -r example/eQTL/region.LOC284581.chr1.205831207.205865215.dosage.p1e-12.LD \
+  -o pyfm_results \
+  -n 471 -c 2 -t 0 -e 0.1 -a 1.6
