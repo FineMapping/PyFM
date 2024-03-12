2eec309f92dfcca3d1d5d36c9fa306ab5cf4f6a5
IshaanSD <i3gupta@ucsd.edu>
2024-03-11 17:12:49 -0700

    cache scores, log them, and black reformat

--- src/configurations.py
+++ src/configurations.py
@@ -4,11 +4,13 @@ import time
 from collections import defaultdict
 from tqdm import tqdm
 import os
+
+
 class Configurations:
     def __init__(self, **kwarg):
         self.max_causal = kwarg["max_causal"]
         self.m = kwarg["m"]
-        self.visited_config_scores = dict() # stores scores of configs already visited
+        self.visited_config_scores = dict()  # stores scores of configs already visited
         self.score_config_f = kwarg[
             "score_config"
         ]  # method to calculate score for a config
@@ -34,36 +36,27 @@ class Configurations:
         do - to be called on the config after every iteration
         """
         tic = time.time()
+        tic_all = tic
         prev_n_causal = self.n_causal
         while not self.ended():
-            if self.n_causal > prev_n_causal: # if n_causal increments, log some stats
-                print(
-                    f"Expored models with {prev_n_causal} causal variant in {time.time() - tic} seconds"
-                )
-                print("n_causal", prev_n_causal)
-                print("best config", self.best_config)
-                print("best score", self.best_score, end='\n\n')
-                tic = time.time()
-                prev_n_causal = self.n_causal
             if do is not None:
                 do(self)
             self.next()
 
         print(
-            f"Expored models with {self.n_causal} causal variant in {time.time() - tic} seconds"
+            f"Expored models up to {self.max_causal} causal variant in {time.time() - tic_all} seconds"
         )
-        print("n_causal", self.n_causal)
+        print("n_causal", len(self.best_config))
         print("best config", self.best_config)
-        print("best score", self.best_score, end='\n\n')
-
+        print("best score", self.best_score, end="\n\n")
 
     def get_scores_by_n_causal(self):
         scores_by_n_causal = defaultdict(dict)
-        for config,score in self.visited_config_scores.items():
+        for config, score in self.visited_config_scores.items():
             scores_by_n_causal[len(config)][config] = score
         return scores_by_n_causal
-    
-    def score_config(self,config):
+
+    def score_config(self, config):
         config_key = tuple(sorted(config))
         if config_key in self.visited_config_scores:
             return self.visited_config_scores[config_key]
@@ -71,6 +64,16 @@ class Configurations:
             score = self.score_config_f(config)
             self.visited_config_scores[config_key] = score
             return score
+
+    def export_scores(self):
+        open(os.path.join(self.outdir, "config_scores.tsv"), "w").close()
+        with open(os.path.join(self.outdir, "config_scores.tsv"), "a") as f:
+            for config, score in sorted(
+                self.visited_config_scores.items(),
+                key=lambda config_score: config_score[1],
+                reverse=True,
+            ):
+                f.write(f"{config}\t{round(score,4)}\n")
 
 
 class AllConfigurations(Configurations):
@@ -127,26 +130,26 @@ class SSSConfigurations(Configurations):
 
     def __init__(self, **kwarg):
         super().__init__(**kwarg)
-        current_causal = (self.max_causal//2) if self.max_causal > 1 else 1
-        self.current_model = np.arange(1,current_causal+1)
-        self.not_included = set(range(current_causal+1, self.m+1))
-        
+        current_causal = (self.max_causal // 2) if self.max_causal > 1 else 1
+        self.current_model = np.arange(1, current_causal + 1)
+        self.not_included = set(range(current_causal + 1, self.m + 1))
+
         self.best_config = self.current_model.copy()
         self.best_score = self.score_config(self.current_model)
-        
-        # Optimization Parameters
-        self.num_iterations = self.optimization_params['SSS_iterations']
-        self.alpha1 = self.optimization_params['SSS_alpha1']
-        self.alpha2 = self.optimization_params['SSS_alpha2']
 
-        np.random.seed(self.optimization_params['Random_Seed'])
+        # Optimization Parameters
+        self.num_iterations = self.optimization_params["SSS_iterations"]
+        self.alpha1 = self.optimization_params["SSS_alpha1"]
+        self.alpha2 = self.optimization_params["SSS_alpha2"]
+
+        np.random.seed(self.optimization_params["Random_Seed"])
 
     def get_negative_neighborhood(self):
         if not len(self.current_model) - 1:
             return []
         copy = self.current_model.copy()[:-1]
         yield copy
-        for i in range(len(self.current_model)-1):
+        for i in range(len(self.current_model) - 1):
             copy[i] = self.current_model[-1]
             yield copy
             copy[i] = self.current_model[i]
@@ -161,7 +164,7 @@ class SSSConfigurations(Configurations):
                     copy[i] = snp
                     yield copy
                     copy[i] = self.current_model[i]
-                    
+
     def get_positive_neighborhood(self):
         if len(self.current_model) == self.max_causal:
             return []
@@ -169,35 +172,56 @@ class SSSConfigurations(Configurations):
         for snp in self.not_included:
             copy[-1] = snp
             yield copy
-    
+
     def sample_from_distribution(self, generator):
         neighbor_scores = np.array([self.score_config(x) for x in generator()])
         if not len(neighbor_scores):
             return None, None
         best_config_index = np.argmax(neighbor_scores)
-        
+
         # Weighted by score
         weights = np.power(neighbor_scores, self.alpha1)
-        
-        random_model_index = np.random.choice(np.arange(len(neighbor_scores)), p=weights/weights.sum())        
-        models = {i: x.copy() for i, x in enumerate(generator()) if i in (best_config_index,random_model_index)}
+
+        random_model_index = np.random.choice(
+            np.arange(len(neighbor_scores)), p=weights / weights.sum()
+        )
+        models = {
+            i: x.copy()
+            for i, x in enumerate(generator())
+            if i in (best_config_index, random_model_index)
+        }
         if neighbor_scores.max() > self.best_score:
             self.best_score = neighbor_scores.max()
             self.best_config = models[best_config_index].copy()
         return models[random_model_index], neighbor_scores[random_model_index]
-        
+
     def search(self):
         tic = time.time()
         for _ in tqdm(range(self.num_iterations)):
-            neighbors = list(map(self.sample_from_distribution, 
-                (self.get_negative_neighborhood, self.get_same_neighborhood, self.get_positive_neighborhood)
-            ))
-            weights = np.power(np.array([neighbor[1] for neighbor in neighbors if neighbor[0] is not None]), self.alpha2)
-            neighbors = [neighbor[0] for neighbor in neighbors if neighbor[0] is not None]
-            next_model_index = np.random.choice(np.arange(len(neighbors)), p=weights/weights.sum())
-            
+            neighbors = list(
+                map(
+                    self.sample_from_distribution,
+                    (
+                        self.get_negative_neighborhood,
+                        self.get_same_neighborhood,
+                        self.get_positive_neighborhood,
+                    ),
+                )
+            )
+            neighbor_scores = [
+                neighbor[1] for neighbor in neighbors if neighbor[0] is not None
+            ]
+            weights = np.power(np.array(neighbor_scores), self.alpha2)
+            neighbors = [
+                neighbor[0] for neighbor in neighbors if neighbor[0] is not None
+            ]
+            next_model_index = np.random.choice(
+                np.arange(len(neighbors)), p=weights / weights.sum()
+            )
+
             self.current_model = neighbors[next_model_index]
-            self.not_included = set(np.arange(1, self.m+1)) - set(self.current_model)
+            self.current_score = neighbor_scores[next_model_index]
+            self.not_included = set(np.arange(1, self.m + 1)) - set(self.current_model)
 
         print(
             f"Expored models up to {self.max_causal} causal variant in {time.time() - tic} seconds"
@@ -205,6 +229,7 @@ class SSSConfigurations(Configurations):
         print("n_causal", len(self.best_config))
         print("best config", np.array(sorted(self.best_config)))
         print("best score", self.best_score)
+
 
 #############################################################
 #        Configurations Factory
